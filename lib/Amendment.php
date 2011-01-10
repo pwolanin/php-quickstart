c7507cfc4422cfa9836977e8cf698f88c92f2976
pwolanin <pwolanin@drupal.org>
2011-01-10 08:51:42 -0500

    Fix Amendment class to populate rate plan data

--- lib/Amendment.php
+++ lib/Amendment.php
@@ -2,4 +2,22 @@
 class Zuora_Amendment extends Zuora_Object
 {
     protected $zType = 'Amendment';
+
+    public function getSoapVar()
+    {
+        if (isset($this->RatePlanData))
+        {
+          $data = array_merge((array)$this->_data, array('RatePlanData'=>$this->RatePlanData->getSoapVar()));
+          return new SoapVar(
+            $data,
+            SOAP_ENC_OBJECT,
+            $this->zType,
+            self::TYPE_NAMESPACE
+          );
+        }
+        else
+        {
+          parent::getSoapVar();
+        }
+    }
 }

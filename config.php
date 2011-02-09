2f76a05a569fe53ac755714cd634821904b5cf72
pwolanin <pwolanin@drupal.org>
2011-02-09 10:02:50 -0500

    update to 28 WSDL

--- config.php
+++ config.php
@@ -29,9 +29,9 @@
 
 $username = '<your username>';
 $password = '<your password>';
-$endpoint = 'https://www.zuora.com/apps/services/a/17.0';
+$endpoint = "https://www.zuora.com/apps/services/a/28.0";
 
 $debug=true;
 $query_batch_size = 100;
-$wsdl='zuora.17.0.wsdl';
+$wsdl='zuora.a.28.0.wsdl';
 ?>

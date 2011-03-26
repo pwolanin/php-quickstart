a9e693223b5453b5c469f83003f9334f809e32d8
pwolanin <pwolanin@49851.no-reply.drupal.org>
2011-03-26 12:18:40 -0400

    Add missing Invoice-related classes

--- lib/Invoice.php
+++ lib/Invoice.php
@@ -3,3 +3,19 @@ class Zuora_Invoice extends Zuora_Object
 {
     protected $zType = 'Invoice';
 }
+
+class Zuora_InvoiceAdjustment extends Zuora_Object
+{
+    protected $zType = 'InvoiceAdjustment';
+}
+
+class Zuora_InvoiceItem extends Zuora_Object
+{
+    protected $zType = 'InvoiceItem';
+}
+
+class Zuora_InvoiceItemAdjustment extends Zuora_Object
+{
+    protected $zType = 'InvoiceItemAdjustment';
+}
+
